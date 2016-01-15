b81f58466af0475e04968d1e12ba9b0967c61414
nikhilJain17 <nikhil.jain0987@gmail.com>
2016-01-14 23:52:23 -0500

    getting values from html

--- Scripts/dischargingmode.js
+++ Scripts/dischargingmode.js
@@ -3,6 +3,16 @@
 // calculate values
 // graph
 
+var vi, vf, r, c;
+
 function dischargeBtnClick() {
-	console.log('Graphinh for discharging');
+	console.log('Graphing for discharging');
+	// snag the values
+	vi = document.getElementById('vi').value;
+	vf = document.getElementById('vf').value;
+	r = document.getElementById('r').value;
+	c = document.getElementById('c').value;
+
+	console.log('\nVI: ' + vi + "\nVF: " + vf + '\nR: ' + r + '\nC: ' + c);
+
 }
