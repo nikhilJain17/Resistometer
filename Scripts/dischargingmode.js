6b15df6dd71fdf7c5c8993b2da673b137da42782
nikhilJain17 <nikhil.jain0987@gmail.com>
2016-01-21 19:29:08 -0500

    fixed discharging script and page

--- Scripts/dischargingmode.js
+++ Scripts/dischargingmode.js
@@ -3,17 +3,38 @@
 // calculate values
 // graph
 
-var vi, vf, r, c;
+var vi, r, c, tau;
+
+var ctx = document.getElementById("myChart").getContext("2d");
+
 
 function dischargeBtnClick() {
 	console.log('Graphing for discharging');
 	// snag the values
 	vi = document.getElementById('vi').value;
-	vf = document.getElementById('vf').value;
+	// vf = document.getElementById('vf').value;
 	r = document.getElementById('r').value;
 	c = document.getElementById('c').value;
 
-	console.log('\nVI: ' + vi + "\nVF: " + vf + '\nR: ' + r + '\nC: ' + c);
+	tau = r * c;
+
+	console.log('\nVI: ' + vi + '\nR: ' + r + '\nC: ' + c + '\nTau: ' + tau);
+
+	for (var i = 0; i < 10; i++) {
+		calculateVc(tau * i);
+	}
+
+
+}
+
+// calculate the stuff
+function calculateVc(time) {
+
+	console.log('\n\nTime over tau: ' + time/tau);
+
+	var vc = vi * Math.pow(Math.E, (-time/tau)); 
+	console.log("Time: " + time + " tau, Vc = " + vc);
+	return vc;
 
 }
 
