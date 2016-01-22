b537125f607165884f072138792dbb0196aeea05
nikhilJain17 <nikhil.jain0987@gmail.com>
2016-01-21 21:19:22 -0500

    discharging can graph!

--- Scripts/dischargingmode.js
+++ Scripts/dischargingmode.js
@@ -2,13 +2,17 @@
 // Get the values in the form
 // calculate values
 // graph
+// require(['Chartjs'], function (Chart) {
+
+// });
 
 var vi, r, c, tau;
 
-var ctx = document.getElementById("myChart").getContext("2d");
-
 
 function dischargeBtnClick() {
+
+	var ctx = document.getElementById("myChart").getContext("2d");
+
 	console.log('Graphing for discharging');
 	// snag the values
 	vi = document.getElementById('vi').value;
@@ -20,9 +24,35 @@ function dischargeBtnClick() {
 
 	console.log('\nVI: ' + vi + '\nR: ' + r + '\nC: ' + c + '\nTau: ' + tau);
 
-	for (var i = 0; i < 10; i++) {
-		calculateVc(tau * i);
-	}
+	// for (var i = 0; i < 10; i++) {
+	// 	calculateVc(tau * i);
+	// }
+
+	var data = {
+
+		labels: ['Tau', '2 Tau', '3 Tau', '4 Tau', '5 Tau', ],
+
+		datasets: [
+			{
+
+				label: 'Vc, Discharging',
+				fillColor: "rgba(151,187,205,0.2)",
+	            strokeColor: "rgba(151,187,205,1)",
+	            pointColor: "rgba(151,187,205,1)",
+	            pointStrokeColor: "#fff",
+	            pointHighlightFill: "#fff",
+	            pointHighlightStroke: "rgba(151,187,205,1)",
+	            data: [ calculateVc(tau), calculateVc(2 * tau), calculateVc(3 * tau), calculateVc(4 * tau), calculateVc(5 * tau) ]
+
+
+			} // end of data set 1
+		] // end of datasets array
+
+	}; // end of data object
+
+	var myLineChart = new Chart(ctx).Line(data, {
+		pointDot : true
+	});
 
 
 }
